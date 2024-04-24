c4df6ffd165b21e0d79cc711fb56cacf826089c2
Kirill <42815480+EloTheWizzard@users.noreply.github.com>
2024-04-24 15:52:43 +0200

    Added: On extension install storage initialization. Changed: Minor popup modes styling changes and refactoring

--- background.js
+++ background.js
@@ -1,3 +1,29 @@
+//Initialize on install
+chrome.runtime.onInstalled.addListener((details) => {
+    //details.reason === "update" is ONLY FOR DEBUGGING!!!
+    if (details.reason === "install" || details.reason === "update") { 
+        console.log("First initialization!")
+        initializeStorage();
+    }
+});
+
+async function initializeStorage() {
+    const storageData = await chrome.storage.local.get();
+    console.log(storageData)
+
+    if (!Object.hasOwn(storageData, "mode")) {
+        chrome.storage.session.set({"mode": "WATCH A FEW MODE"});
+    }
+
+    if (!Object.hasOwn(storageData, "numberOfEscapes")) {
+        chrome.storage.session.set({ "numberOfEscapes": 0})
+    }
+
+    if (!Object.hasOwn(storageData, "watchedVideosCounter")) {
+        chrome.storage.session.set({ "watchedVideosCounter": 0})
+    }
+}
+
 chrome.storage.onChanged.addListener((changes, namespace) => {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         console.log(
@@ -9,13 +35,12 @@ chrome.storage.onChanged.addListener((changes, namespace) => {
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
+        initializeStorage();
         if (request.greeting === "escapedscrolling") {
-            chrome.storage.session.get(["numberOfEscapes"])
+            chrome.storage.local.get(["numberOfEscapes"])
             .then((result) => {
-                updatedEscapes = Object.hasOwn(result, "numberOfEscapes") ? result["numberOfEscapes"] + 1: 0;
-                chrome.storage.session.set({ "numberOfEscapes": updatedEscapes}).then(() => {
-                    sendResponse({farewell: "good!"});
-                });
+                // updatedEscapes = Object.hasOwn(result, "numberOfEscapes") ? result["numberOfEscapes"] + 1: 0;
+                chrome.storage.local.set({ "numberOfEscapes": result + 1});
             })
         }
         if (request.mode) {
@@ -25,11 +50,11 @@ chrome.runtime.onMessage.addListener(
 );
 
 function changeWatchMode(newMode) {
-    chrome.storage.session.get(["mode"]).then((result) => {
-        chrome.storage.session.set({"mode": newMode});
+    chrome.storage.local.get(["mode"]).then((result) => {
+        chrome.storage.local.set({"mode": newMode});
     });
 }
 
-// function trackShortWatches () {
+function trackShortWatches () {
 
-// }
+}
--- popup.js
+++ popup.js
@@ -1,42 +1,52 @@
 'use strict'
 window.addEventListener('load', async function(event) {
+
     await setEscapes();
     await setMode();
 });
 
 async function setEscapes() {
-    const currentEscapes = await getCurrentEscapes();
+    const storage = await getStorageData();
     const counter = document.getElementById('infocontainer-escape-counter');
-    counter.innerHTML = currentEscapes["numberOfEscapes"] + ' LIKES';   
+    console.log(storage)
+    counter.innerHTML = storage.numberOfEscapes + ' TIMES';   
 }
+
+
 
 async function setMode() {
     let buttons = document.getElementsByClassName('popup-modeselector-btn');
 
     chrome.storage.session.get(["mode"]).then((result) => {
         if (!Object.hasOwn(result, "mode")) {
-            chrome.storage.session.set({"mode": "WATCH A FEW MODE"});
+            chrome.storage.local.set({"mode": "WATCH A FEW MODE"});
         }
     });
 
     for (let button = 0; button < buttons.length; button++) {
         buttons[button].onclick = async function() {
+            const radioButton =  buttons[button].querySelector('.mode-selector');
+            radioButton.checked = true;
+            
             const buttonElement = buttons[button].querySelector('.modeselector-text');
             const response = await chrome.runtime.sendMessage({mode: buttonElement.innerHTML});
         }
     }
 }
 
-function getCurrentEscapes() {
-    return new Promise((resolve, reject) => {
-        chrome.storage.session.get(["numberOfEscapes"])
-        .then((result) => {
-            
-            const currentEscapes = Object.hasOwn(result, "numberOfEscapes") ? result : 0; //In case we got undefined, we just set it to
-            console.log(currentEscapes);
-            resolve(currentEscapes);
-        }) 
-    });
+async function getStorageData() {
+    // return new Promise((resolve, reject) => {
+    //     chrome.storage.session.get(["numberOfEscapes"])
+    //     .then((result) => {
+    //         // const currentEscapes = Object.hasOwn(result, "numberOfEscapes") ? result : 0; //In case we got undefined, we just set it to
+    //         console.log(result);
+    //         if (result) resolve(result);
+    //     }) 
+    // });
+
+    const storageData = await chrome.storage.local.get();
+    return storageData;
+    console.log(storageData)
 }
 
 
